4729c2f9094e4e67597ca643220b995ea652b091
Nicolas sarchi <nicolassarchipico@gmail.com>
2023-09-02 00:12:42 -0500

    feat: :sparkles: UnitOfWork Updated
    Se actualiza la interfaz y la implementacion de la unidad de trabajo de acuerdo a los nuevos archivos para la creación de los nuevos controladores

--- Aplicacion/UnitOfWork/UnitOfWork.cs
+++ Aplicacion/UnitOfWork/UnitOfWork.cs
@@ -4,36 +4,75 @@ using Persistencia.Data;
 
 namespace Aplicacion.UnitOfWork;
 
-    public class UnitOfWork : IUnitOfWork, IDisposable
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly Incidenciascontext context;
+    private PersonaRepository _personas;
+    private DepartamentoRepository _departamentos;
+    private PaisRepository _paises;
+    private CiudadRepository _ciudades;
+
+    public UnitOfWork(Incidenciascontext _context)
     {
-        private readonly Incidenciascontext context;
-        private PersonaRepository _personas;
+        context = _context;
+    }
 
-        public UnitOfWork(Incidenciascontext _context)
+    public IPersona Personas
+    {
+        get
         {
-            context = _context;
-        }
-
-        public IPersona Personas
-        {
-            get
+            if (_personas == null)
             {
-                if (_personas == null)
-                {
-                    _personas = new PersonaRepository(context);
-                }
-                return _personas;
+                _personas = new PersonaRepository(context);
             }
-        }
-
-        public async Task<int> SaveAsync()
-        {
-            return  await context.SaveChangesAsync();
-        }
-
-        public void Dispose()
-        {
-            context.Dispose();
+            return _personas;
         }
     }
+
+    public IPais Paises
+    {
+        get
+        {
+            if (_paises == null)
+            {
+                _paises = new PaisRepository(context);
+            }
+            return _paises;
+        }
+    }
+
+    public IDepartamento Departamentos
+    {
+        get
+        {
+            if (_departamentos == null)
+            {
+                _departamentos = new DepartamentoRepository(context);
+            }
+            return _departamentos;
+        }
+    }
+
+    public ICiudad Ciudades
+    {
+        get
+        {
+            if (_ciudades == null)
+            {
+                _ciudades = new CiudadRepository(context);
+            }
+            return _ciudades;
+        }
+    }
+
+    public async Task<int> SaveAsync()
+    {
+        return await context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        context.Dispose();
+    }
+}
 
--- Dominio/Interfaces/IUnitOfWork.cs
+++ Dominio/Interfaces/IUnitOfWork.cs
@@ -3,6 +3,9 @@ namespace Dominio.Interfaces;
 public interface IUnitOfWork 
 {
     IPersona Personas{ get; }
+    IPais Paises{ get; }
+    IDepartamento Departamentos{ get; }
+    ICiudad Ciudades{ get; }
 
     Task<int> SaveAsync();
 }
